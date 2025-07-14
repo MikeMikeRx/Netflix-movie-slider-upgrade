a8d018fccabd679f06916b87a1fe17c51ffa4e24
MichalRais <Michal.Rais@iCloud.com>
2025-07-14 11:05:17 +0100

    Fix issue with display movies

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,12 +2,12 @@ import "./Movies.css"
 import data from "../data"
 
 const Movies = () => {
-    return <div className="all-movies">
+    return <div className="all-films">
         {
             data.map((oneMovie)=>{
                 const {id, image, title, age, tags, description} = oneMovie
 
-                return <div className="one-movie" key={id}>
+                return <div className="one-film" key={id}>
                     <img src={image} alt="" />
                     <h2>{title}</h2>
                     <p>{age}</p>
