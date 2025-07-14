fcebadd4d42c942497c6e0042af1ca57867b3375
MichalRais <Michal.Rais@iCloud.com>
2025-07-14 16:10:03 +0100

    Clean up structure in App.js

--- src/App.js
+++ src/App.js
@@ -5,51 +5,48 @@ import Question from "./components/Question"
 import questions from "./questions"
 
 const App = () => {
-  const [view, setView] = useState("slider")
+  const [view, setView] = useState("movies")
 
 
 
   return (
     <div>
-        <div className="bg">
-          <div className="overlay">
+      <div className="bg">
+                                  
+        <div className="header">
+          
+          <h1 className="main-title">NetfliX</h1>
 
-              <div className="header">
-                <h1 className="main-title">NetfliX</h1>
+          <div className="view-switcher">
 
-                
-                
-                <div className="view-switcher">
-                  <button
-                    className={view === "slider" ? "active" : ""}
-                    onClick={()=> setView("slider")}
-                  >
-                    Slider
-                  </button>
+            <button
+              className={view === "movies" ? "active" : ""}
+              onClick={()=> setView("movies")}
+              >
+                Movies
+            </button>
 
-                  <button
-                  className={view === "movies" ? "active" : ""}
-                  onClick={()=> setView("movies")}
-                  >
-                    Movies
-                  </button>
-                </div>
-              </div>
-
-
-                <div className="slider">    
-                  {view === "slider" ? <OneMovieSlider /> : <Movies />}            
-            </div>
+            <button
+              className={view === "slider" ? "active" : ""}
+              onClick={()=> setView("slider")}
+              >
+                Slider
+            </button>
           </div>
-      </div>       
-            
-      {/* Question section */}
+        </div>
+
+        <div className="slider">    
+          {view === "slider" ? <OneMovieSlider /> : <Movies />}            
+        </div>
+      </div>
+                                      
+                                    
       <div className="faq-wrap">
         <h2 className="faq-title">Frequetly Asked Questions</h2>
           <div className="all-questions">
             {questions.map( (q) => {
-                return <Question key={q.id} {...q}/>
-                })}
+              return <Question key={q.id} {...q}/>
+            })}
           </div>
       </div>
     </div>    
