0e1d0d539fd12a1963e1ed23718865b56e3722b6
MichalRais <Michal.Rais@iCloud.com>
2025-07-15 11:32:41 +0100

    Colors fixed

--- src/components/DummyImg.js
+++ src/components/DummyImg.js
@@ -1,7 +1,7 @@
 const DummyImg = () =>{
     const width = 300
     const height = 160
-    const bgColor = "028500"
+    const bgColor = "00235c"
     const text = "Movie Title"
     const textColor = "ffffff"
 
@@ -15,3 +15,5 @@ const DummyImg = () =>{
 export default DummyImg
 
 
+
+// https://dummyimg.vercel.app/300x160/00235c/ffffff/png?text=Movie%20title&fontsize=
